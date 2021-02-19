3549db96fbb32a456e2586b286253460404603fb
Mathew Biddle <mathew.biddle@noaa.gov>
2021-02-19 15:15:40 -0500

    initial review of PacIOOS added some processing scripts and configs reprocessed output data files

--- utils/temp.py
+++ utils/temp.py
@@ -8,14 +8,16 @@ import json
 with open('ra_erddaps.json') as f:
     urls = json.load(f)
 
-df = pd.read_excel('../2020/data/raw/MARACOOS Asset Inventory_2020_FINAL.xlsx')
-
-df_cruise = df[df['Station Description'] == 'Chesapeake Bay WQ Cruise Data ']
-df_wf = df[df['Station Description'] == 'WeatherFlow Meteorological Station']
+df = pd.read_excel('../2020/data/raw/Asset Inventory_PacIOOS_2020.xlsx',sheet_name=1)
+#
+# df_cruise = df[df['Station Description'] == 'Chesapeake Bay WQ Cruise Data ']
+# df_wf = df[df['Station Description'] == 'WeatherFlow Meteorological Station']
+df_aws = df[df['Station ID'] == 'AWS-HIMB']
 # #url = 'http://tds.glos.us/thredds/dodsC/buoy_agg_standard/OMOECC_E1/OMOECC_E1.ncml'
 # #url = 'http://tds.glos.us/thredds/dodsC/buoy_agg_standard/45186/45186.ncml'
 # #url = 'http://tds.glos.us/thredds/dodsC/buoy_agg_standard/bgsusd2/bgsusd2.ncml'
-# url = 'http://thredds.secoora.org/thredds/dodsC/secoora/sensors/gov_fl_fldep_8722213/data/sea_water_temperature.nc'
+# url = 'http://oos.soest.hawaii.edu/thredds/dodsC/hioos/nss/ns12agg'
+# print(url)
 # ds = xr.open_dataset(url)
 # #ds = netCDF4.Dataset(url,'r')
 # title = ds.title
@@ -33,15 +35,15 @@ df_wf = df[df['Station Description'] == 'WeatherFlow Meteorological Station']
 # check out http://data.glos.us/erddap/tabledap/allDatasets.htmlTable?datasetID%2Ctitle%2CminTime%2CmaxTime&maxTime%3E=2020-01-01&maxTime%3C=2020-12-31&orderBy(%22maxTime%22)
 # that lists out all the GLOS stations with the maximum time of observations within the year 2020 (on their ERDDAP).
 
-server = urls['maracoos']
+server = urls['pacioos']
 e = ERDDAP(server=server, protocol="tabledap")
-for index, row in df_wf.iterrows():
-    id = row['Station Long Name'].lower().replace(', ','_')
+for index, row in df_aws.iterrows():
+    id = row['Station ID']
     e.dataset_id = id
-    # e.constraints = {
-    #     "time>=": "2020-01-01",
-    #     "time<=": "2020-12-31"
-    # }
+    e.constraints = {
+        "time>=": "2020-01-01",
+        "time<=": "2020-12-31"
+    }
     title = e.dataset_id
 # # url = 'http://data.glos.us/erddap/tabledap/osugi.csv'
     df_data = e.to_pandas(parse_dates=True)
