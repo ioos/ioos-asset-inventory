4318ae728b88a1af50e33814e9623fc9aeef94ba
Mathew Biddle <mathew.biddle@noaa.gov>
2021-02-25 12:43:31 -0500

    initial review of caricoos regenerate inventory files

--- utils/temp.py
+++ utils/temp.py
@@ -8,11 +8,11 @@ import json
 with open('ra_erddaps.json') as f:
     urls = json.load(f)
 
-df = pd.read_excel('../2020/data/raw/CeNCOOS Asset Inventory 2019update2122020_02262020_011121.xlsx', sheet_name=0)
+df = pd.read_excel('../2020/data/raw/CARICOOS Asset Inventory_2020.xlsx', sheet_name=0)
 #
 # df_cruise = df[df['Station Description'] == 'Chesapeake Bay WQ Cruise Data ']
-# df_wf = df[df['Station Description'] == 'WeatherFlow Meteorological Station']
-df_aws = df[df['Station ID'] == 'Humboldt']
+#
+#df_aws = df[df['Station ID'] == 'Humboldt']
 # #url = 'http://tds.glos.us/thredds/dodsC/buoy_agg_standard/OMOECC_E1/OMOECC_E1.ncml'
 # #url = 'http://tds.glos.us/thredds/dodsC/buoy_agg_standard/45186/45186.ncml'
 # #url = 'http://tds.glos.us/thredds/dodsC/buoy_agg_standard/bgsusd2/bgsusd2.ncml'
@@ -35,15 +35,19 @@ df_aws = df[df['Station ID'] == 'Humboldt']
 # check out http://data.glos.us/erddap/tabledap/allDatasets.htmlTable?datasetID%2Ctitle%2CminTime%2CmaxTime&maxTime%3E=2020-01-01&maxTime%3C=2020-12-31&orderBy(%22maxTime%22)
 # that lists out all the GLOS stations with the maximum time of observations within the year 2020 (on their ERDDAP).
 
-server = urls['cencoos']
+server = urls['caricoos']
 e = ERDDAP(server=server, protocol="tabledap")
-for index, row in df_aws.iterrows():
-    id = row['Station ID'].replace('Humboldt', 'edu_humboldt_humboldt')
-    e.dataset_id = id
-    e.constraints = {
-        "time>=": "2020-01-01",
-        "time<=": "2020-12-31"
-    }
+search_url = e.get_search_url(search_for="all", response="csv")
+all_datasets = pd.read_csv(search_url)
+df_wf = df[df['Platform Operator/Owner'] == 'Weatherflow']
+
+for index, row in df_wf.iterrows():
+    id = all_datasets.loc[all_datasets['Title'].str.contains(row['Station ID']), ['Dataset ID','Title']]
+    #print(id.item())
+    e.dataset_id = id['Dataset ID']
+    # e.constraints = {
+    #     "time>=": "2020-01-01"
+    # }
     title = e.dataset_id
 # # url = 'http://data.glos.us/erddap/tabledap/osugi.csv'
     df_data = e.to_pandas(parse_dates=True)
@@ -53,7 +57,7 @@ for index, row in df_aws.iterrows():
 # set index for plotting
     df_data = df_data.set_index(df_data['time (UTC)'])
 # plot
-    df_data.plot(subplots=True)
+    df_data.plot(subplots=True, title=title)
 
     start_time = df_data['time (UTC)'].min()
     end_time = df_data['time (UTC)'].max()
